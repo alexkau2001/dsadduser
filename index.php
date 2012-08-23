14baa44d363ce5868b28dd5bcf4f9d5e5e216dd7
Mark Myers <marcusmyers@gmail.com>
2012-08-23 15:27:44 -0400

    Added bootstrap files

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=8" />
+	<title>NACS Treasurers Office :: Bud/Revs</title>
+	<!--  Styles -->
+	<link href="css/bootstrap.min.css" rel="stylesheet" />
+	<link href="css/bootstrap-responsive.min.css" rel="stylesheet" />
+	<link href="css/default.css" rel="stylesheet" />
+	
+	
+	<!--  HTML5 shim, for IE6-8 support of HTML5 Elements -->
+	<!--[if lt IE 9]
+		<script srce="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+<div class="navbar navbar-fixed-top"> 
+	<div class="navbar-inner">
+		<div class="container">
+			<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</a>
+			<a class="brand" href="#">DSAdd Users</a>
+<?php ?>
+		</div>
+	</div>
+</div>
+	<script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
